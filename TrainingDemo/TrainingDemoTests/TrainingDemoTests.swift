9de796f05d236d8817c5448829792de5b5c050c7
RamanpreetS <ramanpreet.singh@mobileprogramming.net>
2018-02-14 11:35:37 -0800

    Update TrainingDemoTests.swift

--- TrainingDemo/TrainingDemoTests/TrainingDemoTests.swift
+++ TrainingDemo/TrainingDemoTests/TrainingDemoTests.swift
@@ -13,7 +13,7 @@ class TrainingDemoTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // jkklhkjl;jk;kj Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
